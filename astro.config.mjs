0161eac80f43900f856566fde8db3171ad53439c
nguyen.linh <onelijn1@gmail.com>
2025-12-15 23:32:16 +0700

    add vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 
 import vue from '@astrojs/vue';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,5 +11,8 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  integrations: [vue()]
+  integrations: [vue()],
+  adapter: vercel({
+    // edge: false, // nếu bạn muốn dùng Serverless Function thay vì Edge Function
+  }),
 });
